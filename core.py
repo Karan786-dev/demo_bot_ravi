23f1dfd3b14d3fec3314c702a461c5036c113da8
Karan786-dev <81469693+Karan786-dev@users.noreply.github.com>
2021-12-30 11:32:15 +0530

    Add files via upload

---
+++ core.py
@@ -0,0 +1,29 @@
+#Bot Token From @BotFather
+bot_token = "5071818034:AAHg0T2M8aqxMYO-7wo82ReiII05OvORemk"
+
+#Admins Telegram ID
+admins = [1468386562, 1871392276]
+
+#MongoDb Url For Databse
+mongo_url = "mongodb+srv://karan:karan@cluster0.hqujv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+
+#Job2All Setup
+merchant_id = "Merchant Id Here"
+merchant_key = "Merchant Key Here"
+subwallet_key = "Subwallet Key Here"
+pay_comment = "Bot Payment| Made By @KaranYTop"
+
+#Earn More Message
+earn_more = '''*Sign up bonus 600 TRX
+
+1️⃣ Level   10%
+2️⃣ Level    6%
+3️⃣ Level    3%
+
+1 Refer 30 TRX
+
+New TRX website link https://www.trx.pub/#/extendReg?id=19179788
+
+Join our community:-
+https://t.me/New_TrxSites*'''
