3da8c84bcf1c799b3438a01c7058b1530ad9a3b9
Karan786-dev <81469693+Karan786-dev@users.noreply.github.com>
2022-01-08 17:39:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -156,7 +156,7 @@ def delete_cha(message):
     t1.start()
 
 
-async def with_2(id, amo):
+def with_2(id, amo):
     merchant_id = get_bot('M_id')
     merchant_key = get_bot('M_key')
     subwallet_key = get_bot('Sub_id')
@@ -182,7 +182,7 @@ async def with_2(id, amo):
 
     oldbal = user_data(id, 'Balance')
     newbal = oldbal - float(amo)
-    await update_user(id, 'Balance', float(newbal))
+    update_user(id, 'Balance', float(newbal))
     bal2 = user_data(id, 'Balance')
     if bal2 < 0:
         return
