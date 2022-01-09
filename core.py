613f50cd568762c3333472ec7c064ef2b3906e46
Karan786-dev <81469693+Karan786-dev@users.noreply.github.com>
2022-01-09 10:19:36 +0530

    Update core.py

--- core.py
+++ core.py
@@ -1,5 +1,5 @@
 #Bot Token From @BotFather
-bot_token = "5020856155:AAHJ_gKzPOj5wO2-uTe8RSyVBL3bwktC_q8"
+bot_token = "5091562857:AAEBR15fAZFOXWP9AwN5K19ib0_pMagcDSQ"
 
 #Admins Telegram ID
 admins = [1468386562,1635788715]
