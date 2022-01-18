4878cdde92c8b20ef151671ae5556819215f1454
Karan786-dev <81469693+Karan786-dev@users.noreply.github.com>
2022-01-18 16:38:14 +0530

    Update core.py

--- core.py
+++ core.py
@@ -1,8 +1,8 @@
 #Bot Token From @BotFather
-bot_token = "5071486120:AAFINRAfUNK6XIPBzawbBF6r13OJXm_w-Ro"
+bot_token = "5074855830:AAEGLk3t5CDX_gIbr2SFLKUoig5yBLWd-MA"
 
 #Admins Telegram ID
-admins = [1468386562]
+admins = [1468386562,1821521027]
 
 #MongoDb Url For Databse
 mongo_url = "mongodb+srv://otto:otto@cluster0.ehnab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
